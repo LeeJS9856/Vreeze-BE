3903a922853ef4b84707b530e782f2edf6e753c3
LeeJS9856 <wnstj1057@naver.com>
2025-02-26 20:31:22 +0900

    Feat : 카카오 로그인 구현

--- Vreeze/settings.py
+++ Vreeze/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
 
+    'social_django',
+
     'user'
 ]
 
@@ -144,3 +146,22 @@ SIMPLE_JWT = {
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN":  True,
 }
+
+# SOCIAL_AUTH 설정
+SOCIAL_AUTH_KAKAO_KEY = secret["KAKAO_KEY"]  # 카카오 앱 키 입력
+SOCIAL_AUTH_KAKAO_SECRET = secret["KAKAO_SECRET"]  # 카카오 앱 시크릿 키 입력
+SOCIAL_AUTH_KAKAO_SCOPE = ['profile_nickname']  # 요청할 정보 범위
+SOCIAL_AUTH_KAKAO_EXTRA_DATA = ['nickname']  # 받아올 추가 정보
+
+
+# 로그인 후 리디렉션 URL 설정
+LOGIN_REDIRECT_URL = 'http://127.0.0.1:8000/auth/complete/kakao/'  # 로그인 후 리디렉션할 URL
+
+# 로그인 오류 시 리디렉션 URL 설정
+LOGOUT_REDIRECT_URL = 'http://127.0.0.1:8000'
+
+# 추가적으로 필요한 설정들
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.kakao.KakaoOAuth2',
+    'django.contrib.auth.backends.ModelBackend',  # 기본 로그인 백엔드
+)

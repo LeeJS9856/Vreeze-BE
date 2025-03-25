8a1d703a9e582a4f901aae8d9aa2fe7e326834fe
LeeJS9856 <wnstj1057@naver.com>
2025-03-25 19:52:57 +0900

    feat : chat 관련 models, serializers, urls, views 추가

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserQuestion(models.Model):
+    question = models.TextField()
+    answer = models.TextField(blank=True, null=True)
+    session_id = models.CharField(max_length=100)
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.exceptions import AuthenticationFailed
+from .models import UserQuestion
+from .serializers import UserQuestionSerializer
+from .ai import ai_chat
 
-# Create your views here.
+class ChatAPIView(APIView):
+    def post(self, request):
+        if not request.user.is_authenticated:
+            raise AuthenticationFailed("로그인이 필요합니다.")
+        question = request.data.get('question')
+        
+        if not request.session.session_key:
+            request.session.create()
+        session_id = request.session.session_key
+
+        if not question:
+            return Response({"error": "질문을 입력해주세요."}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            answer = ai_chat(question,session_id)
+            question_instance = UserQuestion.objects.create(
+                question=question,
+                answer=answer,
+                session_id=session_id
+            )
+            return Response(
+                UserQuestionSerializer(question_instance).data,
+                status=status.HTTP_201_CREATED
+            )
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
